00b0d0d8110200c24ae7a7fa6bc225c0b5f1e51a
Alexander Mihalopoulos <amihalop@gmail.com>
2015-07-27 14:42:20 -0400

    add new, create, and workout_params

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,32 @@
+class WorkoutsController < ApplicationController
+
+  def new
+    user = User.find(params[:user_id])
+    @workout = user.workouts.new
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    @workout = user.workouts.new(workout_params)
+    if @workout.save
+      redirect_to user_path(user)
+    else
+      redirect_to new_user__workout_path(user)
+    end
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :date)
+  end
+
+end

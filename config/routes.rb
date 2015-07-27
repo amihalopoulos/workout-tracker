3be3110f634ad518c4bec46c5ae8922783ba869c
Alexander Mihalopoulos <amihalop@gmail.com>
2015-07-27 14:38:13 -0400

    add routes for current migrations

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :users do
+    resources :workouts do
+      resources :exercises do
+        resources :sets
+      end
+    end
+  end
+
+  resource :session, :only => [:new, :create, :destroy]
+
+  get 'signin' => 'sessions#new', as: 'signin'
+  get 'signout' => 'sessions#destroy', as: 'signout'
+  get 'signup' => 'users#new', as: 'signup'
+
+  root 'application#index'
+end

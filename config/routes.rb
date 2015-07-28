16d8662bebe9e7934e8d1181e8418c02288f2d85
Alexander Mihalopoulos <amihalop@gmail.com>
2015-07-28 15:33:23 -0400

    add routes for rounds remove sets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :workouts do
       resources :exercises do
-        resources :sets
+        resources :rounds
       end
     end
   end

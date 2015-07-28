3f2a67f51518de59e0592887d60acb007d3779be
Alexander Mihalopoulos <amihalop@gmail.com>
2015-07-28 15:28:33 -0400

    first attempt at ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,46 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// $(document).ready(function(){
+//     $(".new_workout").on('click', function(event) {
+//       event.preventDefault();
+
+//       $.ajax({
+//           type: 'POST',
+//           success: function() {
+//             alert("hello")
+//           }
+//       });
+
+//       });
+//     });
+
+var formAjax = {
+  init: function() {
+    $('#new_workout').on('ajax:success', this.appendForm);
+    $('#new_workout').on('ajax:error', this.displayError);
+  },
+
+  appendForm: function() {
+    console.log(arguments)
+  },
+  displayError: function() {
+    console.log(arguments)
+  }
+}
+$(document).ready(function() {
+  formAjax.init();
+//   $('#new_workout').on('submit', function(event){
+//       event.preventDefault();
+//       $form = $(event.target);
+
+//       $.ajax({
+//       url: $form.serialize(),
+//       type: 'post'
+//       // data: {"tweet[content]": $textarea.val(), "hashtags": hashtags}
+//     }).done(function(response){
+//       console.log(response)
+//   })
+// })
+})

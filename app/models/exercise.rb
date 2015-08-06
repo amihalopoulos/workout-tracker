14474edfec0562867fe2f74af037715acf15d4c6
Alexander Mihalopoulos <amihalop@gmail.com>
2015-08-06 11:54:25 -0400

    #one_rep_dataset

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,19 @@ class Exercise < ActiveRecord::Base
   belongs_to :workout
   belongs_to :user
   has_many :rounds
+
+  def get_one_rep_dataset
+    exercises = Exercise.where(name: self.name, user_id: self.user_id)
+    dataset= []
+    exercises.each do |exercise|
+      array_of_avgs = []
+      exercise.rounds.each do |round|
+        array_of_avgs << round.one_rep_average
+      end
+      one_rep_max = (array_of_avgs.inject{ |sum, el| sum + el }.to_f / array_of_avgs.size)
+      dataset << [one_rep_max, exercise.workout.date]
+    end
+    return dataset
+  end
+
 end

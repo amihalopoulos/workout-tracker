6ae6f9c7d16fdefb08fc7030dc0e9dfa51411b8b
Alexander Mihalopoulos <amihalop@gmail.com>
2015-08-13 17:26:13 -0400

    redirect if user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    redirect_to user_path(current_user) if current_user
+  end
+
   def current_user
     User.find_by(id: session[:user_id]) if session[:user_id]
   end
